bb1be1d2e5e1a40d0c04f175d5c8fd604fa21a8b
OilPainter1 <hjcryns@gmail.com>
2022-11-10 14:40:35 -0600

    added /notes and /api/notes routes to server.js

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,13 +1,36 @@
 const express = require("express")
+const { get } = require("http")
 const path = require("path")
+const fs = require('fs')
 const app = express()
 const port = 3002
+
+app.use(express.static("/Users/henrycryns/Desktop/Note-Taker/Develop/public/assets/css/styles.css"))
 
 app.get("/",(req,res)=>{
     res.sendFile("/Users/henrycryns/Desktop/Note-Taker/Develop/public/index.html")
     res.end
     
 })
+app.get("/notes", (req,res)=>{
+    res.sendFile("/Users/henrycryns/Desktop/Note-Taker/Develop/public/notes.html")
+    res.end
+})
+app.get("/api/notes", (req,res)=>{
+    res.json(JSON.parse(fs.readFileSync("/Users/henrycryns/Desktop/Note-Taker/Develop/db/db.json","utf-8",(err,data)=>{
+        if(err){
+            console.log(err)
+            return
+        }else{
+            return data
+        }
+    })))
+})
+app.get("*", (req,res)=>{
+    res.sendFile("/Users/henrycryns/Desktop/Note-Taker/Develop/public/index.html")
+    res.end
+})
+
 app.listen(port,()=>{
     console.log(`server listening on port ${port}`)
     
